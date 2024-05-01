f4ec125b410a01225e3e0690d7653d5ace1689bc
Pramudya <pramudyap333@gmail.com>
2024-05-01 22:59:57 +0700

    new update

--- app/Http/Controllers/MobileAPI/TransactionController.php
+++ app/Http/Controllers/MobileAPI/TransactionController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers\MobileAPI;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Details;
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     public function showPesananIn(Request $request) {
         // show data pesanan
-    }
+        try {
+            $transactions = Transaksi::with('details.product')->get();;
+
+
+            if ($transactions) {
+                // return 
+                return response()->json(['status' => 'success', 'message' => 'status succes', 'data' => $transactions], 200);
+            } else {
+                
+                return response()->json(['status' => 'fail', 'message' => 'failed'], 200);
+            }
+            
+        } catch (\Exception $e) {
+            
+            return response()->json(['status' => 'failure', 'message' => $e], 500);
+        }
+    }   
 
     public function updatePesanan(Request $request) {
         $status = $request->input('status');
---
+++ app/Models/Transaksi.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'transaksi';
+
+    protected $primaryKey = "kode_transaksi";
+
+    protected $fillable = [
+        "nama",
+        "status",
+        "jumlah",
+        "total",
+        "update"
+    ];
+
+    // Define the relationship with details
+    public function details()
+    {
+        return $this->hasMany(DetailTransaksi::class, 'kode_transaksi', 'kode_transaksi');
+    }
+}
